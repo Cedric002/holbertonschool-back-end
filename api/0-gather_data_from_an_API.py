063722761b249dfe6ff1869a70cc51272b459ccc
Cedric002 <cedgnleblanc@gmail.com>
2024-04-15 11:28:52 +0200

    pep8 gather-data-from-api-py

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
 "Returns information employee ID"
 
-import requests  # type: ignore
+import requests
 
 
 def todo_list_progress(employee_id):
 
-    employee = requests.get(f'https://jsonplaceholder.typicode.com/users/{employee_id}').json()
-    todos = requests.get(f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos').json()
+    url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
+    employee = requests.get(url).json()
+
+    url = f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos'
+    todos = requests.get(url).json()
 
     total_tasks = len(todos)
     done_tasks = len([todo for todo in todos if todo['completed']])
     employee_name = employee['name']
 
-    print(f'Employee {employee_name} is done with tasks({done_tasks}/{total_tasks}):')
+    print(f'Employee {employee_name} is done with tasks'
+          f'({done_tasks}/{total_tasks}):')
 
     for todo in todos:
         if todo['completed']:
