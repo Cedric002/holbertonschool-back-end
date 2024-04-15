e3d6ab8a22c6b7b2159ff071b5b74d8f0ea63826
Cedric002 <cedgnleblanc@gmail.com>
2024-04-15 11:08:44 +0200

    add gather-data-from-api-py

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"Returns information employee ID"
+
+import requests # type: ignore
+
+
+def todo_list_progress(employee_id):
+    employee = requests.get(f'https://jsonplaceholder.typicode.com/users/{employee_id}').json()
+    todos = requests.get(f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos').json()
+
+    total_tasks = len(todos)
+    done_tasks = len([todo for todo in todos if todo['completed']])
+    employee_name = employee['name']
+
+    print(f'Employee {employee_name} is done with tasks({done_tasks}/{total_tasks}):')
+
+    for todo in todos:
+        if todo['completed']:
+            print('\t ' + todo['title'])
+
+if __name__ == "__main__":
+
+
+    todo_list_progress(1)
