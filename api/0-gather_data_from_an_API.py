a927cd123cfba86bb381b398b19666e3354d9809
Cedric002 <cedgnleblanc@gmail.com>
2024-04-15 15:54:50 +0200

    fix gather-py and export-CSV-py

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -2,28 +2,31 @@
 "Returns information employee ID"
 
 import requests
+import sys
 
 
-def todo_list_progress(employee_id):
+def get_employee_todo_list(employee_id):
 
-    url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
-    employee = requests.get(url).json()
+    user_response = requests.get(
+        f'https://jsonplaceholder.typicode.com/users/{employee_id}')
+    todos_response = requests.get(
+        f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos')
 
-    url = f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos'
-    todos = requests.get(url).json()
+    user_data = user_response.json()
+    todos_data = todos_response.json()
 
-    total_tasks = len(todos)
-    done_tasks = len([todo for todo in todos if todo['completed']])
-    employee_name = employee['name']
+    employee_name = user_data.get('name')
+
+    done_tasks = [task for task in todos_data if task.get('completed')]
 
     print(f'Employee {employee_name} is done with tasks'
-          f'({done_tasks}/{total_tasks}):')
+          f'({len(done_tasks)}/{len(todos_data)}):')
 
-    for todo in todos:
-        if todo['completed']:
-            print('\t ' + todo['title'])
+    for task in done_tasks:
+        print('\t ' + task.get('title'))
 
 
 if __name__ == "__main__":
 
-    todo_list_progress()
+    employee_id = int(sys.argv[1])
+    get_employee_todo_list(employee_id)
--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -5,35 +5,30 @@
 
 import csv
 import requests
+import sys
 
 
-def todo_list_progress(employee_id):
+def export_employee_todo_list(employee_id):
 
-    url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
-    employee = requests.get(url).json()
+    user_response = requests.get(
+        f'https://jsonplaceholder.typicode.com/users/{employee_id}')
+    todos_response = requests.get(
+        f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos')
 
-    url = f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos'
-    todos = requests.get(url).json()
+    user_data = user_response.json()
+    todos_data = todos_response.json()
+    employee_name = user_data.get('name')
 
-    total_tasks = len(todos)
-    done_tasks = len([todo for todo in todos if todo['completed']])
-    employee_name = employee['name']
-
-    print(f'Employee {employee_name} is done with tasks'
-          f'({done_tasks}/{total_tasks}):')
+    rows = [[employee_id, employee_name, task.get('completed'),
+             task.get('title')]
+            for task in todos_data]
 
     with open(f'{employee_id}.csv', 'w', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow(["USER_ID", "USERNAME",
-                         "TASK_COMPLETED_STATUS", "TASK_TITLE"])
-
-        for todo in todos:
-            writer.writerow([employee_id, employee_name,
-                             todo['completed'], todo['title']])
-            if todo['completed']:
-                print('\t ' + todo['title'])
+        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+        writer.writerows(rows)
 
 
 if __name__ == "__main__":
 
-    todo_list_progress()
+    employee_id = int(sys.argv[1])
+    export_employee_todo_list(employee_id)
