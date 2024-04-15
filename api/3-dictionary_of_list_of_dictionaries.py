60edf511b89b098002578effd31dfd7fdfdc5aae
Cedric002 <cedgnleblanc@gmail.com>
2024-04-15 15:39:10 +0200

    old dictionary-of-list-of-dictionaries-py

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,25 +1,34 @@
 #!/usr/bin/python3
 
-import requests
+"Returns information employee ID"
+"Export in JSON format method"
+
 import json
-import sys
+import requests
+
 
 def todo_list_progress(employee_id):
+
     base_url = 'https://jsonplaceholder.typicode.com/users'
-    employee = requests.get(f'{base_url}/{employee_id}').json()
-    todos = requests.get(f'{base_url}/{employee_id}/todos').json()
-    tasks = []
-    for todo in todos:
-        task = {"username": employee["name"], "task": todo['title'],
-                "completed": todo['completed']}
-        tasks.append(task)
-        
-    with open(f'{employee_id}.json', 'w') as file:
-        json.dump({employee_id: tasks}, file)
+    all_users = requests.get(base_url).json()
+    all_tasks = {}
+
+    for user in all_users:
+        employee_id = user['id']
+        employee_name = user['name']
+        todos = requests.get(f'{base_url}/{employee_id}/todos').json()
+        tasks = []
+
+        for todo in todos:
+            task = {"username": employee_name, "task": todo['title'],
+                    "completed": todo['completed']}
+            tasks.append(task)
+        all_tasks[employee_id] = tasks
+
+    with open('todo_all_employees.json', 'w') as file:
+        json.dump(all_tasks, file)
+
 
 if __name__ == "__main__":
 
-    if len(sys.argv) > 1:
-        todo_list_progress(sys.argv[1])
-    else:
-        print("Employee's ID")
+    todo_list_progress()
