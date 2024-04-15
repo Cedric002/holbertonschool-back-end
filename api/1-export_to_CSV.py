96282bbe238969d3f74336a1783d7697b68c2834
Cedric002 <cedgnleblanc@gmail.com>
2024-04-15 14:50:26 +0200

    add export_to_CSV-py

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 "Returns information employee ID"
 
+import csv
 import json
 import requests
 import sys
@@ -21,9 +22,16 @@ def todo_list_progress(employee_id):
     print(f'Employee {employee_name} is done with tasks'
           f'({done_tasks}/{total_tasks}):')
 
-    for todo in todos:
-        if todo['completed']:
-            print('\t ' + todo['title'])
+    with open(f'{employee_id}.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(["USER_ID", "USERNAME",
+                         "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+
+        for todo in todos:
+            writer.writerow([employee_id, employee_name,
+                             todo['completed'], todo['title']])
+            if todo['completed']:
+                print('\t ' + todo['title'])
 
 
 if __name__ == "__main__":
