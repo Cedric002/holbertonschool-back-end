f2fe04bc2a82da08a3349c74a8105c03e067f34c
Cedric002 <cedgnleblanc@gmail.com>
2024-04-16 11:25:01 +0200

    update dictionary

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,35 +1,45 @@
 #!/usr/bin/python3
 
-import json
-import requests
 
 "Returns all tasks of all employees"
 "Export in JSON format method"
 
 
+import json
+import requests
+import sys
+
+
 def export_all_employees_todo_list():
 
-    users_response = requests.get('https://jsonplaceholder.typicode.com/users')
-    todos_response = requests.get('https://jsonplaceholder.typicode.com/todos')
+    user_response = requests.get('https://jsonplaceholder.typicode.com/users')
+    user_data = user_response.json()
 
-    users_data = users_response.json()
-    todos_data = todos_response.json()
+    all_employee_tasks = {}
 
-    all_tasks = {}
-    for user in users_data:
-        user_tasks = [
-            {
-                "username": user.get('name'),
-                "task": task.get('title'),
-                "completed": task.get('completed')
-            }
-            for task in todos_data if task.get('userId') == user.get('id')
-        ]
-        all_tasks[user.get('id')] = user_tasks
+    for user in user_data:
+        user_id = user['id']
+        username = user['username']
+        todos_response = requests.get(
+            f'https://jsonplaceholder.typicode.com/users/{user_id}/todos')
+        todos_data = todos_response.json()
 
-    with open('todo_all_employees.json', 'w') as file:
-        json.dump(all_tasks, file)
+        employee_tasks = []
+        for task in todos_data:
+            employee_tasks.append({
+                "username": username,
+                "task": task['title'],
+                "completed": task['completed']
+            })
+
+        all_employee_tasks[user_id] = employee_tasks
+
+    with open('todo_all_employees.json', 'w') as f:
+        json.dump(all_employee_tasks, f, indent=4)
+
+    print("Data exported to 'todo_all_employees.json'")
 
 
 if __name__ == "__main__":
+
     export_all_employees_todo_list()
