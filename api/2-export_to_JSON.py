ad331575cf14782db3eb6fbfbf3582f01437925f
Cedric002 <cedgnleblanc@gmail.com>
2024-04-16 11:17:43 +0200

    update export to Json

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
-import json
-import requests
-import sys
 
 "Returns information employee ID"
 "Export in JSON format method"
+
+import json
+import requests
+import sys
 
 
 def export_employee_todo_list(employee_id):
@@ -17,17 +18,28 @@ def export_employee_todo_list(employee_id):
 
     user_data = user_response.json()
     todos_data = todos_response.json()
+
     employee_name = user_data.get('name')
 
-    tasks = [{"task": task.get('title'), "completed":
-              task.get('completed'), "username": employee_name}
-             for task in todos_data]
+    tasks = [
+        {
+            "task": task.get('title'),
+            "completed": task.get('completed'),
+            "username": employee_name
+        } for task in todos_data
+    ]
 
-    with open(f'{employee_id}.json', 'w') as file:
-        json.dump({employee_id: tasks}, file)
+    print(f'Employee {employee_name} has {len(tasks)} tasks:')
 
+    for task in tasks:
+        print('\t ' + task.get('task'))
+
+    return {str(employee_id): tasks}
 
 if __name__ == "__main__":
 
     employee_id = int(sys.argv[1])
-    export_employee_todo_list(employee_id)
+    employee_tasks = export_employee_todo_list(employee_id)
+
+    with open(f"{employee_id}.json", "w") as f:
+        json.dump(employee_tasks, f, indent=4)
